50788884b8581017b1a7011233647038dca12743
Abhishekabh99 <abhishekabh99@gmail.com>
2023-08-28 22:16:40 +0530

    modifications in pattern

--- Pattern.java
+++ Pattern.java
@@ -1,8 +1,12 @@
 public class Pattern {
     public static void main(String[] args) {
+        System.out.println("square pattern below :");
         square();
+        System.out.println("right angle traingle below :");
+        rat();
     }
 
+    // square pattern
     public static void square() {
         int i = 0;
         while (i < 5) {
@@ -15,4 +19,20 @@ public class Pattern {
             i++;
         }
     }
+
+    // right angle traingle
+    public static void rat() {
+        int i = 0;
+
+        while (i < 5) {
+            int j = 0;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+    //
 }
